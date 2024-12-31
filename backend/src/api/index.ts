04ef70d3712f35448a8ad6fde54b931b9a96834c
kalubekalu <kalubekalu1@gmail.com>
2024-12-31 13:07:19 -0600

    added all api's

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import questionRoutes from './routes/questionRoutes';
+import userRoutes from './routes/userRoutes';
+import topicRoutes from './routes/topicRoutes';
+import quizRoutes from './routes/quizRoutes';
 
 const router = Router();
 
 router.use('/questions', questionRoutes);
+router.use('/users', userRoutes);
+router.use('/topics', topicRoutes);
+router.use('/quizzes', quizRoutes);
 
 export { router };
